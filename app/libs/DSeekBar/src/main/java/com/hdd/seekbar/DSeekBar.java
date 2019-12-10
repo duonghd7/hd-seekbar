f75d80f58a244545f6c0c270dc93ba09046a7f60
duonghd <duonghd1@kaopiz.com>
2019-12-10 19:42:28 +0700

    Update

--- app/libs/DSeekBar/src/main/java/com/hdd/seekbar/DSeekBar.java
+++ app/libs/DSeekBar/src/main/java/com/hdd/seekbar/DSeekBar.java
@@ -1,5 +1,35 @@
 package com.hdd.seekbar;
 
-public class DSeekBar {
-    
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class DSeekBar extends FrameLayout {
+
+    public DSeekBar(@NonNull Context context) {
+        super(context);
+        initDSeekBar(context);
+    }
+
+    public DSeekBar(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initDSeekBar(context);
+    }
+
+    public DSeekBar(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initDSeekBar(context);
+    }
+
+    public DSeekBar(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        initDSeekBar(context);
+    }
+
+    private void initDSeekBar(@NonNull Context context) {
+
+    }
 }
